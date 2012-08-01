f0fa3af2be41d67414d3dbcee0aa077cb81f24ef
Dale Tan <wtdtan@gmail.com>
2012-08-01 08:37:12 -0400

    added global nav data

---
+++ routes/data.js
@@ -0,0 +1,43 @@
+
+module.exports = {
+/*
+	<div id="utils">
+		<ul class="nonstick">
+		<li><a href="#" class="util-login">Login</a></li>
+		<li><a href="#" class="util-signup">Sign Up</a></li>
+		</ul>
+		<ul class="sticky">
+				<li><a href="#" class="util-profile">Profile</a></li>
+				<li><a href="#" class="util-nav">Navigation</a></li>
+				<li><a href="#" class="util-search">Search</a></li>
+			</ul>
+		</div>
+*/
+
+	"nav": {
+		"login": {
+			"title": "Login",
+			"href": "#"
+		},
+		"signup": {
+			"title": "Sign Up",
+			"href": "#"
+		},
+		"profile": {
+			"title": "Profile",
+			"href": "#",
+			"is-sticky": true
+		},
+		"nav": {
+			"title": "Navigation",
+			"href": "#",
+			"is-sticky": true
+		},
+		"search": {
+			"title": "Search",
+			"href": "#",
+			"is-sticky": true
+		}
+	}
+}
+	 
--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,11 @@
 
+var data = require('./data');
+
 /*
  * GET home page.
  */
 
 exports.index = function(req, res){
-  res.render('index', { title: 'Express' })
+	console.log('data: ', data);
+	res.render('index', { title: 'Express', data: data })
 };
